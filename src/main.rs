42334d17b4a9589b05e6c5d0d7826dbce04886c4
Evil Terminal <27122323+Ev1lT3rm1nal@users.noreply.github.com>
2023-02-28 19:00:15 -0600

    Better implementation (not working)

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 use std::env;
 use std::io::{stdin, stdout, Write};
-use std::process::Command;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+use which::{which, which_all};
 
 fn main() {
     // Comprobar si Chocolatey está instalado
 
     println!("Comprobando si Chocolatey está instalado...");
-    let output = Command::new("powershell")
-        .arg("Get-Command choco")
-        .output()
-        .expect("Error al ejecutar el comando 'powershell'");
+    let output = which("choco");
 
-    if !output.status.success() {
+    if output.is_err() {
         // Chocolatey no está instalado, instalarlo
         println!("Chocolatey no está instalado, instalando...");
         let install_command = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))";
 
-        Command::new("powershell")
+        let mut process = Command::new("powershell")
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .arg(install_command)
-            .output()
+            .spawn()
             .expect("Error al ejecutar el comando de instalación de Chocolatey");
+
+        process.wait().expect("Error al correr el comando");
 
         // Añadir Chocolatey al PATH
         env::set_var(
@@ -32,7 +35,74 @@ fn main() {
     }
 
     // Comprobar si OpenJDK 17 está instalado
-    check_and_install("java", "openjdk17 --version=17.0.2", "OpenJDK 17");
+
+    // Obtiene todos los ejecutables en el PATH de java y ve si existe JAVA_HOME
+    let executables = which_all("java");
+
+    // comprueba si alguno de los ejecutables es openjdk 17
+    let mut executable_path: Option<PathBuf> = None;
+
+    if let Ok(executables) = executables {
+        for executable in executables {
+            let process = Command::new("powershell")
+                .stdout(Stdio::piped())
+                .args([format!(".\"{}\"", executable.display()).as_str(), "-version"])
+                .spawn()
+                .expect("Error al ejecutar el comando 'java -version'");
+
+            let output = process
+                .wait_with_output()
+                .expect("Error al esperar la salida del comando");
+
+            if output.status.success() {
+                let output = String::from_utf8_lossy(&output.stdout).to_string();
+
+                if output.contains("openjdk version \"17") {
+                    executable_path = Some(executable);
+                    break;
+                }
+
+                println!("{}", output);
+            }
+        }
+    }
+
+    /*
+       En caso de que si haya, comproba si JAVA_HOME está configurado y es el mismo que el directorio de OpenJDK 17
+       En caso de que no, configúralo
+       En caso de que no, instala OpenJDK 17
+    */
+
+    if let Some(executable) = executable_path {
+        // get java.home it through java -XshowSettings:properties -version
+        let output = Command::new(executable)
+            .arg("-XshowSettings:properties")
+            .arg("-version")
+            .output()
+            .expect("Error al obtener la propiedad 'java.home'");
+
+        let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+
+        let java_home = stdout.split("java.home = ").collect::<Vec<&str>>()[1]
+            .split("")
+            .collect::<Vec<&str>>()[0]
+            .to_string();
+
+        print!("java.home = {}", java_home);
+
+        if java_home.is_empty() || java_home != java_home {
+            // Configurar JAVA_HOME
+            println!("Configurando JAVA_HOME...");
+
+            // setx Java_Home "C:\Program Files\Java\jdk-17.0.1"
+            Command::new("powershell")
+                .arg(format!("setx JAVA_HOME \"{}\"", java_home))
+                .output()
+                .expect("Error al ejecutar el comando 'setx JAVA_HOME'");
+        }
+    } else {
+        install("openjdk17 --version=17.0.2", "OpenJDK 17");
+    }
 
     // Comprobar si Maven está instalado
     check_and_install("mvn", "maven --version=3.8.4", "Maven");
@@ -40,15 +110,13 @@ fn main() {
     // Comprobar si Postgres está instalado
     println!("Comprobando si Postgres está instalado...");
 
-    let output = Command::new("powershell")
-        .arg("Get-Command psql")
-        .output()
-        .expect("Error al ejecutar el comando 'powershell'");
+    let output = which("psql");
 
-    if !output.status.success() {
+    if output.is_err() {
         // use gui installer from https://sbp.enterprisedb.com/getfile.jsp?fileid=1258323 with reqwest
         // Postgres no está instalado, instalarlo
         println!("Postgres no está instalado, instalando...");
+        println!("Descargando instalador de Postgres...");
         let target = "https://sbp.enterprisedb.com/getfile.jsp?fileid=1258323";
         let mut resp = reqwest::blocking::get(target).unwrap();
         let mut file = std::fs::File::create("postgres.exe").unwrap();
@@ -56,7 +124,7 @@ fn main() {
 
         file.flush().unwrap();
         file.sync_all().unwrap();
-        //stop file being used
+        // para de usar el archivo
         drop(file);
 
         Command::new("postgres.exe")
@@ -67,16 +135,13 @@ fn main() {
     // Comprobar si Git está instalado
     check_and_install("git", "git", "Git");
 
-    // check and install nodejs lts
+    // Comprobar si NodeJS está instalado
     check_and_install("node", "nodejs-lts", "NodeJS");
 
     // Comprobar si Visual Studio Code está instalado
-    let output = Command::new("powershell")
-        .arg("Get-Command code")
-        .output()
-        .expect("Error al ejecutar el comando 'powershell'");
+    let output = which("code");
 
-    if !output.status.success() {
+    if output.is_err() {
         // Preguntar si desea instalar Visual Studio Code
         let mut input = String::new();
         print!("¿Desea instalar Visual Studio Code? (s/n) ");
@@ -87,13 +152,7 @@ fn main() {
 
         if input.trim().to_lowercase() == "s" {
             // Instalar Visual Studio Code
-            println!("Instalando Visual Studio Code...");
-            let install_command = "choco install -y vscode";
-
-            Command::new("powershell")
-                .arg(install_command)
-                .output()
-                .expect("Error al ejecutar el comando de instalación de Visual Studio Code");
+            install("vscode", "Visual Studio Code");
         }
     }
 
@@ -107,12 +166,9 @@ fn main() {
     );
 
     // verificar si "code" está en el path
-    let output = Command::new("powershell")
-        .arg("Get-Command code")
-        .output()
-        .expect("Error al ejecutar el comando 'powershell'");
+    let output = which("code");
 
-    if output.status.success() {
+    if output.is_ok() {
         // Preguntar si desea instalar plugins para Visual Studio Code para Java y Spring Boot
         let mut input = String::new();
         print!("¿Desea instalar plugins para Visual Studio Code para Java y Spring Boot? (s/n) ");
@@ -128,10 +184,14 @@ fn main() {
             // install vscjava.vscode-spring-initializr and vscjava.vscode-java-pack
             let install_command = "code --install-extension vscjava.vscode-spring-initializr --install-extension vscjava.vscode-java-pack";
 
-            Command::new("powershell")
+            let mut process = Command::new("powershell")
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
                 .arg(install_command)
-                .output()
+                .spawn()
                 .expect("Error al ejecutar el comando de instalación de plugins para Visual Studio Code para Java y Spring Boot");
+
+            process.wait().expect("Error al correr el comando");
         }
     }
 
@@ -145,17 +205,10 @@ fn main() {
 
     if input.trim().to_lowercase() == "s" {
         // Instalar IntelliJ IDEA Community Edition
-        println!("Instalando IntelliJ IDEA Community Edition...");
-        let install_command = "choco install -y intellijidea-community";
-
-        Command::new("powershell")
-            .arg(install_command)
-            .output()
-            .expect("Error al ejecutar el comando de instalación de IntelliJ IDEA Community Edition");
+        install("intellijidea-community", "IntelliJ IDEA Community Edition");
     }
-    
 
-    // pause the console
+    // pausa
     let mut input = String::new();
     print!("Presione ENTER para continuar...");
     let _ = stdout().flush();
@@ -167,19 +220,36 @@ fn main() {
 fn check_and_install(cmd: &str, install_cmd: &str, name: &str) {
     println!("Comprobando si {} está instalado...", name);
 
-    let output = Command::new("powershell")
-        .arg(format!("Get-Command {}", cmd))
-        .output()
-        .expect("Error al ejecutar el comando 'powershell'");
+    let output = which(cmd);
 
-    if !output.status.success() {
+    if output.is_err() {
         // cmd no está instalado, instalarlo
-        println!("{} no está instalado, instalando...", name);
-        let install_command = format!("choco install -y {}", install_cmd);
+        install(install_cmd, name);
+    }
+}
 
-        Command::new("powershell")
-            .arg(install_command)
-            .output()
-            .expect(format!("Error al ejecutar el comando de instalación de {}", name).as_str());
+fn install(install_cmd: &str, name: &str) {
+    println!("{} no está instalado, instalando...", name);
+    let install_command = format!("choco install -y {}", install_cmd);
+
+    let process = Command::new("powershell")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .arg(install_command)
+        .spawn()
+        .expect(format!("Error al ejecutar el comando de instalación de {}", name).as_str());
+
+    let out = process
+        .wait_with_output()
+        .expect("Error al correr el comando");
+
+    if !out.status.success() {
+        println!("Error al instalar {}", name);
+        // show output
+        let stdout = out.stdout;
+        let stderr = out.stderr;
+
+        println!("stdout: {}", String::from_utf8_lossy(&stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&stderr));
     }
 }
